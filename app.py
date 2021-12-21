9a3e0f2dfd31e1fae874c10be4b929843487cdfd
AustinJamesWolff <austinwolff1997@gmail.com>
2021-12-21 02:03:15 -0800

    Restructured directory for purposes of deploying on heroku

---
+++ app.py
@@ -0,0 +1,31 @@
+"""This app will let the user input a song and return the top 5 
+most similar songs using a KNN model and the Spotify API."""
+
+from flask import Flask, render_template, request
+import pickle
+
+
+app = Flask(__name__)
+
+# Below will load the pickled-model
+# TODO: UNCOMMENT BELOW WHEN WE HAVE THE MODEL
+# with open(pickled_model_name, 'rb') as file:
+#     KNN_Model = pickle.load(file)
+
+def get_similar_songs(your_song):
+    """Use the KKN model on the user's song input."""
+    # TODO: USE THE KNN MODEL TO PREDICT TOP 5 MOST
+    # SIMILAR SONGS FROM THE SPOTIFY DATASET
+    return None
+
+@app.route('/', methods=['GET','POST'])
+def root():
+    """The home page."""
+    # your_song=request.form
+    if request.method == 'POST':
+        your_song=request.form
+    return render_template('index.html', your_song=your_song)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
